bb4b471b7dc80f230d8a45fd852911bd2274165f
mhouston8 <111707834+mhouston8@users.noreply.github.com>
2023-07-18 10:43:59 -0700

    added launch screen via storyboard.

--- DocuPulse/View/Onboarding/OnboardingView.swift
+++ DocuPulse/View/Onboarding/OnboardingView.swift
@@ -10,59 +10,73 @@ import SwiftUI
 struct OnboardingView: View {
     
     var body: some View {
-        VStack {
-            ZStack {
-                Image("Onboarding-Main-Background")
-                    .resizable()
-                    .scaledToFit()
-                
-                Image("Phone Mock")
-                    .resizable()
-                    .scaledToFit()
-            }
-            .padding(.top, 35)
-            
+        
+        GeometryReader { geometry in
             VStack {
-                Text("Scan all your documents")
-                    .font(.system(size: 23, weight: .bold))
-                    .bold()
-                    .padding(.bottom, 5)
-                Text("quickly and easily")
-                    .font(.system(size: 23, weight: .bold))
-                    .bold()
-                    .padding(.bottom, 5)
-                
-                Text("Conveniently scan, save, and share in minutes. Scan anywhere with ease.")
-                    .font(.system(size: 15, weight: .light))
-                
-                HStack(spacing: 17) {
-                    Button {
-                        print("Skip pressed")
-                    } label: {
-                        Text("Skip")
-                            .padding()
-                            .padding(.horizontal, 40)
-                            .foregroundColor(Color("darkPurple"))
-                            .background(Color("lightPurple"))
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                VStack {
+                    ZStack(alignment: .bottom) {
+                        Image("Onboarding-Main-Background")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.bottom, 50)
+                        
+                        Image("Phone Mock")
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Spacer()
+                            .frame(width: geometry.size.width, height: 150)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.6), Color.white.opacity(1.0)]),
+                                                       startPoint: .top,
+                                                       endPoint: .bottom)
+                                            .edgesIgnoringSafeArea(.all))
+                        
                     }
+                    .padding(.top, 35)
+                }
+                
+                VStack {
+                    Text("Scan all your documents")
+                        .font(.system(size: 23, weight: .bold))
+                        .bold()
+                        .padding(.bottom, 5)
+                    Text("quickly and easily")
+                        .font(.system(size: 23, weight: .bold))
+                        .bold()
+                        .padding(.bottom, 5)
                     
-                    Button {
-                        print("Next pressed")
-                    } label: {
-                        Text("Next")
-                            .padding()
-                            .padding(.horizontal, 40)
-                            .foregroundColor(.white)
-                            .background(Color("darkPurple"))
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                    }
+                    Text("Conveniently scan, save, and share in minutes. Scan anywhere with ease.")
+                        .font(.system(size: 15, weight: .light))
+                    
+                    HStack(spacing: 17) {
+                        Button {
+                            print("Skip pressed")
+                        } label: {
+                            Text("Skip")
+                                .padding()
+                                .padding(.horizontal, 40)
+                                .foregroundColor(Color("darkPurple"))
+                                .background(Color("lightPurple"))
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                        
+                        Button {
+                            print("Next pressed")
+                        } label: {
+                            Text("Next")
+                                .padding()
+                                .padding(.horizontal, 40)
+                                .foregroundColor(.white)
+                                .background(Color("darkPurple"))
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
 
+                    }
+                    .padding()
                 }
                 .padding()
+                Spacer()
             }
-            .padding()
-            Spacer()
         }
     }
 }
