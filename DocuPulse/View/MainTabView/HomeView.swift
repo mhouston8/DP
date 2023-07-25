a90205bc2ca08989801c299c0daea5462cd397f4
mhouston8 <111707834+mhouston8@users.noreply.github.com>
2023-07-24 20:49:48 -0700

    Added ZStack to Home View

--- DocuPulse/View/MainTabView/HomeView.swift
+++ DocuPulse/View/MainTabView/HomeView.swift
@@ -11,155 +11,162 @@ struct HomeView: View {
     var body: some View {
         
         NavigationView {
-            VStack {
-                
-                
-                HStack {
-                    Text("DocuPulse")
-                        .font(.largeTitle).bold()
-                    Spacer()
-                    Image(systemName: "magnifyingglass")
-                }
-                
-                VStack(alignment: .leading) {
-                    HStack(spacing: 40) {
-                        VStack {
-                            Image("Scan Code")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("Scan Code")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
-                        VStack {
-                            Image("Watermark")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("Watermark")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
-                        VStack {
-                            Image("eSign PDF")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("eSign PDF")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
-                        VStack {
-                            Image("Split PDF")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("Split PDF")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
+            ZStack(alignment: .bottomTrailing) {
+                VStack {
+                    HStack {
+                        Text("DocuPulse")
+                            .font(.largeTitle).bold()
+                        Spacer()
+                        Image(systemName: "magnifyingglass")
                     }
-                    .padding(.top)
                     
-                    HStack(spacing: 40) {
-                        VStack {
-                            Image("Merge PDF")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("Merge PDF")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
-                        VStack {
-                            Image("Protect PDF")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("Protect PDF")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
-                        VStack {
-                            Image("Compress PDF")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("Compress PDF")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
-                        VStack {
-                            Image("All Tools")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                            Text("All Tools")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
-                    }
-                    .padding(.top)
-                    
-                    Divider()
-                        .padding(.top)
-                }
-                .padding([.leading, .trailing])
-                
-                
-                //Recent Files
-                HStack {
-                    Text("Recent Files")
-                        .bold()
-                        .font(.system(size: 25))
-                    Spacer()
-                    Image(systemName: "arrow.right")
-                }
-                .padding()
-                
-                ScrollView {
-                    VStack {
-                        ForEach(0..<15) { document in
-                            HStack {
-                                Image("Document")
+                    VStack(alignment: .leading) {
+                        HStack(spacing: 40) {
+                            VStack {
+                                Image("Scan Code")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(height: 100)
-                                
-                                VStack(alignment: .leading, spacing: 10) {
-                                    Text("Job Application")
-                                        .bold()
-                                    Text("Letter")
-                                        .bold()
-                                    Text("10/12/23 12:34pm")
-                                        .font(.system(size: 12, weight: .light))
+                                    .frame(width: 50, height: 50)
+                                Text("Scan Code")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                            VStack {
+                                Image("Watermark")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                Text("Watermark")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                            VStack {
+                                Image("eSign PDF")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                Text("eSign PDF")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                            VStack {
+                                Image("Split PDF")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                Text("Split PDF")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                        }
+                        .padding(.top)
+                        
+                        HStack(spacing: 40) {
+                            VStack {
+                                Image("Merge PDF")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                Text("Merge PDF")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                            VStack {
+                                Image("Protect PDF")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                Text("Protect PDF")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                            VStack {
+                                Image("Compress PDF")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                Text("Compress PDF")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                            VStack {
+                                Image("All Tools")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                Text("All Tools")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                        }
+                        .padding(.top)
+                        
+                        Divider()
+                            .padding(.top)
+                    }
+                    .padding([.leading, .trailing])
+                    
+                    
+                    //Recent Files
+                    HStack {
+                        Text("Recent Files")
+                            .bold()
+                            .font(.system(size: 25))
+                        Spacer()
+                        Image(systemName: "arrow.right")
+
+                    }
+                    .padding()
+                    
+                    ScrollView {
+                        VStack {
+                            ForEach(0..<15) { document in
+                                HStack {
+                                    Image("Document")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 100)
+                                    
+                                    VStack(alignment: .leading, spacing: 10) {
+                                        Text("Job Application")
+                                            .bold()
+                                        Text("Letter")
+                                            .bold()
+                                        Text("10/12/23 12:34pm")
+                                            .font(.system(size: 12, weight: .light))
+                                    }
+                                    .padding()
+                                    
+                                    HStack {
+                                        Image("Share")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 25, height: 25)
+                                        Image("Ellipsis")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 25, height: 25)
+                                    }
                                 }
                                 .padding()
-                                
-                                HStack {
-                                    Image("Share")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 25, height: 25)
-                                    Image("Ellipsis")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 25, height: 25)
-                                }
+                                .background(Color(.systemGroupedBackground))
                             }
-                            .padding()
-                            .background(Color(.systemGroupedBackground))
                         }
                     }
+                    
+                    Spacer()
                 }
+                .padding([.top, .leading, .trailing])
+                .navigationBarHidden(true)
                 
-                Spacer()
+                HStack {
+                    Image(systemName: "camera")
+                        .frame(width: 25, height: 25)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 75 / 255, green: 104 / 255, blue: 255 / 255))
+                        .clipShape(Circle())
+                    Image(systemName: "photo")
+                        .frame(width: 25, height: 25)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 75 / 255, green: 104 / 255, blue: 255 / 255))
+                        .clipShape(Circle())
+                }
+                .padding(30)
             }
-            .padding([.top, .leading, .trailing])
-            .navigationBarHidden(true)
-//            .navigationTitle("DocuPulse")
-//                .toolbar {
-//                    ToolbarItem(placement: .navigationBarTrailing) {
-//                        Button {
-//                            print("search")
-//                        } label: {
-//                            Image(systemName: "magnifyingglass")
-//                        }
-//                    }
-//                }
         }
     }
 }
