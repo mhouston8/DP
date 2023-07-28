886d09afb855fffe2da56c997c5a938189d14ff1
mhouston8 <111707834+mhouston8@users.noreply.github.com>
2023-07-27 23:59:28 -0700

    added files view

---
+++ DocuPulse/View/MainTabView/FilesView.swift
@@ -0,0 +1,70 @@
+//
+//  FilesView.swift
+//  DocuPulse
+//
+//  Created by Matthew Houston on 7/27/23.
+//
+
+import SwiftUI
+
+struct FilesView: View {
+    var body: some View {
+        
+        NavigationView {
+            VStack {
+                //Nav bar
+                HStack {
+                    Image("DocuPulse Icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                        
+                    
+                    Text("Files")
+                        .font(.title2)
+                    Spacer()
+                    
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                        .padding(.horizontal)
+                    
+                    Image(systemName: "ellipsis.circle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                }
+                .padding()
+                .padding(.trailing)
+                
+                HStack {
+                    Text("Total: 125 files")
+                        .bold()
+                    Spacer()
+                    Image(systemName: "arrow.up.arrow.down")
+                        .padding(.horizontal)
+                    Image(systemName: "plus.rectangle.on.folder")
+                }
+                .padding()
+                .padding(.trailing)
+                
+                ScrollView {
+                    VStack {
+                        ForEach(0..<15) { document in
+                            DocumentCell()
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+    }
+}
+
+struct FilesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilesView()
+    }
+}
--- DocuPulse/View/MainTabView/MainTabView.swift
+++ DocuPulse/View/MainTabView/MainTabView.swift
@@ -12,46 +12,44 @@ struct MainTabView: View {
     @State var selectedIndex = 0
     
     var body: some View {
-        NavigationView {
-            TabView(selection: $selectedIndex) {
-                HomeView()
-                    .tabItem {
-                        Label("Home", systemImage: "house")
-                            .onTapGesture {
-                                selectedIndex = 0
-                            }
-                    }
-                    .tag(0)
-                
-                HomeView()
-                    .tabItem {
-                        Label("Files", systemImage: "folder")
-                            .onTapGesture {
-                                selectedIndex = 1
-                            }
-                    }
-                    .tag(1)
-                
-                HomeView()
-                    .tabItem {
-                        Label("Premium", systemImage: "star")
-                            .onTapGesture {
-                                selectedIndex = 2
-                            }
-                    }
-                    .tag(2) 
-                
-                HomeView()
-                    .tabItem {
-                        Label("Account", systemImage: "person.crop.circle")
-                            .onTapGesture {
-                                selectedIndex = 3
-                            }
-                    }
-                    .tag(3)
+        TabView(selection: $selectedIndex) {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                        .onTapGesture {
+                            selectedIndex = 0
+                        }
+                }
+                .tag(0)
+            
+            FilesView()
+                .tabItem {
+                    Label("Files", systemImage: "folder")
+                        .onTapGesture {
+                            selectedIndex = 1
+                        }
+                }
+                .tag(1)
+            
+            HomeView()
+                .tabItem {
+                    Label("Premium", systemImage: "star")
+                        .onTapGesture {
+                            selectedIndex = 2
+                        }
+                }
+                .tag(2)
+            
+            HomeView()
+                .tabItem {
+                    Label("Account", systemImage: "person.crop.circle")
+                        .onTapGesture {
+                            selectedIndex = 3
+                        }
+                }
+                .tag(3)
             }
         }
-    }
 }
 
 struct MainTabView_Previews: PreviewProvider {
