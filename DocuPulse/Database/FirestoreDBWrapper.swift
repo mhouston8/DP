d7125abeab318d3e876f23085ecf2bb49633a408
Matthew Houston <matthew.houston7@icloud.com>
2023-11-19 15:24:12 -0800

    database updates

--- DocuPulse/Database/FirestoreDBWrapper.swift
+++ DocuPulse/Database/FirestoreDBWrapper.swift
@@ -12,39 +12,42 @@ import FirebaseDatabase
 import FirebaseStorage
 
 class FirebaseDBWrapper {
-    var databaseReference = Firestore.firestore()
+    let databaseReference = Firestore.firestore()
+    let storageReference = Storage.storage().reference()
     
     func saveDocument(document: UIImage) {
         let deviceID = UIDevice.current.identifierForVendor?.uuidString
-        
-        guard let imageData = document.jpegData(compressionQuality: 0.75) else {
-            // Handle the error if the image data could not be obtained
-            return
-        }
+        guard let imageData = document.jpegData(compressionQuality: 1.00) else { return }
 
-        let storageRef = Storage.storage().reference().child("document/myImage.jpg")
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        let currentDateString = dateFormatter.string(from: date)
+        let documentsStoragePathReference = self.storageReference.child("documents/\(currentDateString).pdf")
         
-        storageRef.putData(imageData, metadata: nil) { metadata, error in
+        
+        documentsStoragePathReference.putData(imageData, metadata: nil) { metadata, error in
             guard let metadata = metadata else {
                 // Handle any errors
                 return
             }
+            
 
-            // Image uploaded successfully
-            // You can also retrieve the download URL if needed
-            storageRef.downloadURL { (url, error) in
+            // Document uploaded successfully
+            documentsStoragePathReference.downloadURL { (url, error) in
                 guard let downloadURL = url else {
                     // Handle error
                     return
                 }
                 
+                
+                //save file path in database
                 let documentData: [String: Any] = ["document": downloadURL.absoluteString]
                 let userDocumentsPathRef = self.databaseReference.collection("users").document(deviceID!).collection("documents").document()
                 userDocumentsPathRef.setData(documentData) { error in
                     if let error = error {
-                        print("Error writing document path: \(error)")
+                        print("Error writing document path to database: \(error)")
                     } else {
-                        print("Document path successfully written!")
+                        print("Document path successfully written to database")
                     }
                 }
             }
